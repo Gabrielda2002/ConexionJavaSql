0b6674fcd483b6faf8899da3a1f07a828d127738
Gabriel Duarte <duartearias2002@gmail.com>
2023-09-28 18:28:59 -0500

    agregando CRUD incompleto

--- StudentSystem/src/main/java/com/gcda/dao/StudentDao.java
+++ StudentSystem/src/main/java/com/gcda/dao/StudentDao.java
@@ -5,6 +5,7 @@ import static com.gcda.connection.ConnectionDB.getConnectionDB;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import com.gcda.domain.Student;
@@ -48,8 +49,54 @@ public class StudentDao {
         ResultSet rs;
         Connection con = getConnectionDB();
         String sql = "SELECT * FROM student";
+
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Student student = new Student();
+                student.setNombre(rs.getString("firts_name"));
+
+                student.setApellido(rs.getString("last_name"));
+
+                student.setTelefono(rs.getString("phone"));
+
+                student.setEmail(rs.getString("emial"));
+
+                student.setId_Student(rs.getInt("idStudent"));
+                students.add(student);
+            }
+        } catch (SQLException e) {
+            System.out.println("Ocurrio un error al realizar la busqueda. "+ e.getMessage());
+        }
         
         return students;
+    }
+
+    public void actualizar(Student student){
+        Connection con = getConnectionDB();
+        String sql = "UPDATE student SET firts_name =?, last_name=?,phone=?, email =?";
+
+        try(PreparedStatement ps=con.prepareStatement(sql)){
+            ps.setString(1, student.getNombre());
+            ps.setString(1,student.getApellido());
+            ps.setString(1, student.getTelefono());
+            ps.setString(1,student.getEmail());
+            ps.setInt(1,student.getId_Student());
+            ps.executeUpdate();
+        }catch(SQLException e){
+            System.out.println("Ocurrio un error en  la actualizacion de datos. "+e.getMessage());
+        }
+    }
+    public void eliminar(int id_estudiante){
+        Connection con = getConnectionDB();
+        String sql = "UPDATE FROM student Where idStudent =?";
+        try(PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1,id_estudiante);
+            ps.executeQuery();
+        } catch (SQLException e) {
+            System.out.println("Ocurrio un error en la eliminacion de datos. " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
